6041867b6978bcdb663300b8ec96fc772a9cdaa2
Jessica Fernandez <jessicafernandez@Jessicas-MacBook-Pro.local>
2020-12-06 19:19:04 -0800

    demographics chart

--- StarterCode copy/app.js
+++ StarterCode copy/app.js
@@ -1,3 +1,4 @@
+function Plots(id) {
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
 d3.json("static/samples.json").then((data)=>{
@@ -77,6 +78,54 @@ var data2 = [trace2];
 // create the bubble plot
 Plotly.newPlot("bubble", data2, layout2); 
 
-
-
 })
+}
+
+function getDemographics(id) {
+    // read the json file to get data
+        d3.json("static/samples.json").then((data)=> {
+    // get the metadata info for the demographic panel
+            var metadata = data.metadata;
+            console.log(metadata)
+    
+          // filter meta data info by id
+           var results = metadata.filter(meta => meta.id.toString() === id)[0];
+          // select demographic panel to put data
+           var demographics = d3.select("#sample-metadata");
+            
+         // empty the demographic info panel each time before getting new id info
+           demographics.html("");
+    
+         // grab the necessary demographic data data for the id and append the info to the panel
+            Object.entries(results).forEach((key) => {   
+                demographics.append("h4").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+            });
+        });
+    }
+    // create the function for the change event
+    function optionChanged(id) {
+        Plots(id);
+        getDemographics(id);
+    }
+    
+    // create the function for the initial data rendering
+    function init() {
+        // select dropdown menu 
+        var dropdown = d3.select("#selDataset");
+    
+        // read the data 
+        d3.json("static/samples.json").then((data)=> {
+            console.log(data)
+    
+            // get the id data to the dropdwown menu
+            data.names.forEach(function(name) {
+                dropdown.append("option").text(name).property("value");
+            });
+    
+            // call the functions to display the data and the plots to the page
+            Plots(data.names[0]);
+            getDemographics(data.names[0]);
+        });
+    }
+    
+    init();
